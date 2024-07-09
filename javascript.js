1e0ca8a4a2af82b6ab17aa43e2cf3c254801b482
Jimmy Wu <jimmywu063@gmail.com>
2024-07-09 15:49:48 -0700

    Created choice functions for human and computer, initialized scores for both

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
-console.log("Hello, World!")
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let choice = Math.round(Math.random() * 2);
+    switch(choice){
+        case 0:
+            return "rock";
+            break;
+
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
+}
+
+function getHumanChoice(){
+    let choice = prompt("Enter Choice:\nRock, Paper, or Scissors");
+    console.log(choice.toLowerCase())
+}
+
+function playRound(humanChoice, computerChoice){
+
+}
+
+getHumanChoice();
