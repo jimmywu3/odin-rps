96e486f0fd8eeaf7b3863a20dfbac5e91abac9c7
Jimmy Wu <jimmywu063@gmail.com>
2024-07-09 20:22:24 -0700

    Completed javascript.js, added function playGame and playRound

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice(){
     let choice = Math.round(Math.random() * 2);
     switch(choice){
@@ -11,6 +8,7 @@ function getComputerChoice(){
         case 1:
             return "paper";
             break;
+
         case 2:
             return "scissors";
             break;
@@ -19,11 +17,66 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let choice = prompt("Enter Choice:\nRock, Paper, or Scissors");
-    console.log(choice.toLowerCase())
+    return choice;
 }
 
-function playRound(humanChoice, computerChoice){
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice){
+        switch(humanChoice){
+            case "rock": 
+                if(computerChoice == "paper"){
+                    computerScore++;
+                    console.log(`YOU LOSE! ${computerChoice} beats ${humanChoice}`)
+                }
+                else if(computerChoice == "scissors"){
+                    humanScore++;
+                    console.log(`YOU WIN! ${humanChoice} beats ${computerChoice}`)
+                }
+                else
+                    console.log("TIED!")
+            break;
+    
+            case "paper":
+                if(computerChoice == "scissors"){
+                    computerScore++;
+                    console.log(`YOU LOSE! ${computerChoice} beats ${humanChoice}`)
+                }
+                else if(computerChoice == "rock"){
+                    humanScore++;
+                    console.log(`YOU WIN! ${humanChoice} beats ${computerChoice}`)
+                }
+                else
+                    console.log("TIED!")
+            break;
+    
+            case "scissors":
+                if(computerChoice == "rock"){
+                    computerScore++;
+                    console.log(`YOU LOSE! ${computerChoice} beats ${humanChoice}`)
+                }
+                else if(computerChoice == "paper"){
+                    humanScore++;
+                    console.log(`YOU WIN! ${humanChoice} beats ${computerChoice}`)
+                }
+                else
+                    console.log("TIED!")
+            break;
+        }
+    }
+
+    for(let i = 0; i < 5; ++i){
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if(humanScore == computerScore){
+        console.log(`You tied. ${humanScore}-${computerScore}`)
+    }else{
+        (humanScore > computerScore) ? console.log(`You Won! ${humanScore}-${computerScore}`) : console.log(`You Lost. ${humanScore}-${computerScore}`);
+    }
 
 }
 
-getHumanChoice();
+playGame();
