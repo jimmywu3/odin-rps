74f6024abc974a9628b8c16cf1abd84c30f46064
Jimmy Wu <jimmywu063@gmail.com>
2024-08-12 18:14:28 -0700

    Created buttons for rps in index.html, deleted function that prompted for player choice in javascript.js, Created eventlisteners for the rps buttons

--- javascript.js
+++ javascript.js
@@ -15,68 +15,53 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let choice = prompt("Enter Choice:\nRock, Paper, or Scissors");
-    return choice;
+function playRound(humanChoice, computerChoice){
+    switch(humanChoice){
+        case "rock": 
+            if(computerChoice == "paper"){
+                computerScore++;
+                console.log(`YOU LOSE! ${computerChoice} beats ${humanChoice}`)
+            }
+            else if(computerChoice == "scissors"){
+                humanScore++;
+                console.log(`YOU WIN! ${humanChoice} beats ${computerChoice}`)
+            }
+            else
+                console.log("TIED!")
+        break;
+
+        case "paper":
+            if(computerChoice == "scissors"){
+                computerScore++;
+                console.log(`YOU LOSE! ${computerChoice} beats ${humanChoice}`)
+            }
+            else if(computerChoice == "rock"){
+                humanScore++;
+                console.log(`YOU WIN! ${humanChoice} beats ${computerChoice}`)
+            }
+            else
+                console.log("TIED!")
+        break;
+
+        case "scissors":
+            if(computerChoice == "rock"){
+                computerScore++;
+                console.log(`YOU LOSE! ${computerChoice} beats ${humanChoice}`)
+            }
+            else if(computerChoice == "paper"){
+                humanScore++;
+                console.log(`YOU WIN! ${humanChoice} beats ${computerChoice}`)
+            }
+            else
+                console.log("TIED!")
+        break;
+    }
 }
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-    function playRound(humanChoice, computerChoice){
-        switch(humanChoice){
-            case "rock": 
-                if(computerChoice == "paper"){
-                    computerScore++;
-                    console.log(`YOU LOSE! ${computerChoice} beats ${humanChoice}`)
-                }
-                else if(computerChoice == "scissors"){
-                    humanScore++;
-                    console.log(`YOU WIN! ${humanChoice} beats ${computerChoice}`)
-                }
-                else
-                    console.log("TIED!")
-            break;
-    
-            case "paper":
-                if(computerChoice == "scissors"){
-                    computerScore++;
-                    console.log(`YOU LOSE! ${computerChoice} beats ${humanChoice}`)
-                }
-                else if(computerChoice == "rock"){
-                    humanScore++;
-                    console.log(`YOU WIN! ${humanChoice} beats ${computerChoice}`)
-                }
-                else
-                    console.log("TIED!")
-            break;
-    
-            case "scissors":
-                if(computerChoice == "rock"){
-                    computerScore++;
-                    console.log(`YOU LOSE! ${computerChoice} beats ${humanChoice}`)
-                }
-                else if(computerChoice == "paper"){
-                    humanScore++;
-                    console.log(`YOU WIN! ${humanChoice} beats ${computerChoice}`)
-                }
-                else
-                    console.log("TIED!")
-            break;
-        }
-    }
-
-    for(let i = 0; i < 5; ++i){
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if(humanScore == computerScore){
-        console.log(`You tied. ${humanScore}-${computerScore}`)
-    }else{
-        (humanScore > computerScore) ? console.log(`You Won! ${humanScore}-${computerScore}`) : console.log(`You Lost. ${humanScore}-${computerScore}`);
-    }
-
-}
-
-playGame();
+rock.addEventListener("click", playRound("rock", getComputerChoice()))
+paper.addEventListener("click", playRound("paper", getComputerChoice()))
+scissors.addEventListener("click", playRound("scissors", getComputerChoice()))
