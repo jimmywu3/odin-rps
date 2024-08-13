ef2bc8189b8f8ef8e8596a6ca076a35bdc252243
Jimmy Wu <jimmywu063@gmail.com>
2024-08-12 23:23:27 -0700

    Changed styles.css to look better, Completed javascript.js stops when a player reaches 5 wins and displays a win message, Added a break line in index.html to make code look cleaner.

--- javascript.js
+++ javascript.js
@@ -2,6 +2,14 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 const results = document.querySelector("#results");
+const playerWins = document.querySelector("#playerScore")
+const computerWins = document.querySelector("#computerScore")
+
+results.textContent = "Welcome to Rock Paper Scissors!"
+
+playerWins.textContent = "Player's Score: 0";
+computerWins.textContent = "Computer's Score: 0";
+
 
 function getComputerChoice(){
     let choice = Math.round(Math.random() * 2);
@@ -20,14 +28,20 @@ function getComputerChoice(){
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
+
 function playRound(humanChoice, computerChoice){
     switch(humanChoice){
         case "rock": 
             if(computerChoice == "paper"){
                 results.textContent = `YOU LOSE! ${computerChoice} beats ${humanChoice}`;
+                computerScore++;
             }
             else if(computerChoice == "scissors"){
                 results.textContent = `YOU WIN! ${humanChoice} beats ${computerChoice}`;
+                playerScore++;
             }
             else
                 results.textContent = "TIED!";
@@ -36,9 +50,11 @@ function playRound(humanChoice, computerChoice){
         case "paper":
             if(computerChoice == "scissors"){
                 results.textContent = `YOU LOSE! ${computerChoice} beats ${humanChoice}`;
+                computerScore++;
             }
             else if(computerChoice == "rock"){
                 results.textContent = `YOU WIN! ${humanChoice} beats ${computerChoice}`;
+                playerScore++;
             }
             else
                 results.textContent = "TIED!";
@@ -47,16 +63,34 @@ function playRound(humanChoice, computerChoice){
         case "scissors":
             if(computerChoice == "rock"){
                 results.textContent = `YOU LOSE! ${computerChoice} beats ${humanChoice}`;
+                computerScore++;
             }
             else if(computerChoice == "paper"){
                 results.textContent = `YOU WIN! ${humanChoice} beats ${computerChoice}`;
+                playerScore++;
             }
             else
                 results.textContent = "TIED!";
         break;
     }
+
+    playerWins.textContent = `Player's Score: ${playerScore}`;
+    computerWins.textContent = `Computer's Score: ${computerScore}`;
+
+    if(playerScore >= 5 || computerScore >= 5){
+        if(playerScore == 5) results.textContent = "Congrats you beat the Computer!";
+        if(computerScore == 5) results.textContent = "Computer beat you, better luck next time :("
+        rock.removeEventListener("click", playRock);
+        paper.removeEventListener("click", playPaper);
+        scissors.removeEventListener("click", playScissors);
+    }
 }
 
-rock.addEventListener("click", () => playRound("rock", getComputerChoice()))
-paper.addEventListener("click", () => playRound("paper", getComputerChoice()))
-scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()))
+const playRock = () => playRound("rock", getComputerChoice());
+const playPaper = () => playRound("paper", getComputerChoice());
+const playScissors = () => playRound("scissors", getComputerChoice());
+
+
+rock.addEventListener("click", playRock);
+paper.addEventListener("click", playPaper);
+scissors.addEventListener("click", playScissors);
